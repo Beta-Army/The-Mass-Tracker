5c540dfd14345887905f0f6ea60f65f4cd723e65
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2020-08-23 19:57:28 +0530

    Connected form to firebase

---
+++ app.js
@@ -0,0 +1,43 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBsb5pxW97kb-p4sKsZGMOKK2WOCGjvHNw",
+    authDomain: "themasstracker.firebaseapp.com",
+    databaseURL: "https://themasstracker.firebaseio.com",
+    projectId: "themasstracker",
+    storageBucket: "themasstracker.appspot.com",
+    messagingSenderId: "383038531913",
+    appId: "1:383038531913:web:4361a9f73608f81ef25298"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Reference contactInfo collections
+let contactInfo = firebase.database().ref("infos");
+
+// Listen for a submit
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  //   Get input Values
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  let message = document.querySelector("#message").value;
+  console.log(name, email, message);
+
+  saveContactInfo(name, email, message);
+
+  document.querySelector(".contact-form").reset();
+}
+
+// Save infos to Firebase
+function saveContactInfo(name, email, message) {
+  let newContactInfo = contactInfo.push();
+
+  newContactInfo.set({
+    name: name,
+    email: email,
+    message: message
+  });
+}
